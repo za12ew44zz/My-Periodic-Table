791e6a4ca5db2e3f9ac99aa5a6a32f4d65b22442
za12ew44zz <za12ew44zz_999@hotmail.com>
2024-02-15 11:30:58 +0700

    add siri sound

---
+++ script.js
@@ -0,0 +1,1037 @@
+function playHydrogenSound(){
+  var sound = new Howl({
+      src: ['./sound/H.mp3']
+    });
+   
+  sound.play()
+}
+
+function playHeliumSound(){
+  var sound = new Howl({
+      src: ['./sound/HE.mp3']
+    });
+   
+  sound.play()
+}
+
+function playLiSound(){
+  var sound = new Howl({
+      src: ['./sound/Li.mp3']
+    });
+   
+  sound.play()
+}
+
+function playBeSound(){
+  var sound = new Howl({
+      src: ['./sound/Be.mp3']
+    });
+   
+  sound.play()
+}
+
+function playBSound(){
+  var sound = new Howl({
+      src: ['./sound/B.mp3']
+    });
+   
+  sound.play()
+}
+
+function playCSound(){
+  var sound = new Howl({
+      src: ['./sound/C.mp3']
+    });
+   
+  sound.play()
+}
+
+function playNSound(){
+  var sound = new Howl({
+      src: ['./sound/N.mp3']
+    });
+   
+  sound.play()
+}
+function playOSound(){
+  var sound = new Howl({
+      src: ['./sound/O.mp3']
+    });
+   
+  sound.play()
+}
+
+function playFSound(){
+  var sound = new Howl({
+      src: ['./sound/F.mp3']
+    });
+   
+  sound.play()
+}
+
+function playNeSound(){
+  var sound = new Howl({
+      src: ['./sound/Ne.mp3']
+    });
+   
+  sound.play()
+}
+
+function playNaSound(){
+  var sound = new Howl({
+      src: ['./sound/Na.mp3']
+    });
+   
+  sound.play()
+}
+
+function playMgSound(){
+  var sound = new Howl({
+      src: ['./sound/Mg.mp3']
+    });
+   
+  sound.play()
+}
+function playAlSound(){
+  var sound = new Howl({
+      src: ['./sound/Al.mp3']
+    });
+   
+  sound.play()
+}
+
+function playSiSound(){
+  var sound = new Howl({
+      src: ['./sound/Si.mp3']
+    });
+   
+  sound.play()
+}
+
+function playPSound(){
+  var sound = new Howl({
+      src: ['./sound/P.mp3']
+    });
+   
+  sound.play()
+}
+
+function playSSound(){
+  var sound = new Howl({
+      src: ['./sound/S.mp3']
+    });
+   
+  sound.play()
+}
+function playClSound(){
+  var sound = new Howl({
+      src: ['./sound/Cl.mp3']
+    });
+   
+  sound.play()
+}
+function playArSound(){
+  var sound = new Howl({
+      src: ['./sound/Ar.mp3']
+    });
+   
+  sound.play()
+}
+
+function playKSound(){
+  var sound = new Howl({
+      src: ['./sound/K.mp3']
+    });
+   
+  sound.play()
+}
+
+function playCaSound(){
+  var sound = new Howl({
+      src: ['./sound/Ca.mp3']
+    });
+   
+  sound.play()
+}
+
+function playScSound(){
+  var sound = new Howl({
+      src: ['./sound/Sc.mp3']
+    });
+   
+  sound.play()
+}
+
+function playTiSound(){
+  var sound = new Howl({
+      src: ['./sound/Ti.mp3']
+    });
+   
+  sound.play()
+}
+
+function playVSound(){
+  var sound = new Howl({
+      src: ['./sound/V.mp3']
+    });
+   
+  sound.play()
+}
+
+
+function playCrSound(){
+  var sound = new Howl({
+      src: ['./sound/Cr.mp3']
+    });
+   
+  sound.play()
+}
+
+function playMnSound(){
+  var sound = new Howl({
+      src: ['./sound/Mn.mp3']
+    });
+   
+  sound.play()
+}
+function playFeSound(){
+  var sound = new Howl({
+      src: ['./sound/Fe.mp3']
+    });
+   
+  sound.play()
+}
+
+function playCoSound(){
+  var sound = new Howl({
+      src: ['./sound/Co.mp3']
+    });
+   
+  sound.play()
+}
+
+function playNiSound(){
+  var sound = new Howl({
+      src: ['./sound/Ni.mp3']
+    });
+   
+  sound.play()
+}
+
+function playCuSound(){
+  var sound = new Howl({
+      src: ['./sound/Cu.mp3']
+    });
+   
+  sound.play()
+}
+
+
+function playZnSound(){
+  var sound = new Howl({
+      src: ['./sound/Zn.mp3']
+    });
+   
+  sound.play()
+}
+
+function playGaSound(){
+  var sound = new Howl({
+      src: ['./sound/Ga.mp3']
+    });
+   
+  sound.play()
+}
+
+function playGeSound(){
+  var sound = new Howl({
+      src: ['./sound/Ge.mp3']
+    });
+   
+  sound.play()
+}
+
+function playAsSound(){
+  var sound = new Howl({
+      src: ['./sound/As.mp3']
+    });
+   
+  sound.play()
+}
+function playSeSound(){
+  var sound = new Howl({
+      src: ['./sound/Se.mp3']
+    });
+   
+  sound.play()
+}
+
+function playBrSound(){
+  var sound = new Howl({
+      src: ['./sound/Br.mp3']
+    });
+   
+  sound.play()
+}
+
+function playKrSound(){
+  var sound = new Howl({
+      src: ['./sound/Kr.mp3']
+    });
+   
+  sound.play()
+}
+function playRbSound(){
+  var sound = new Howl({
+      src: ['./sound/Rb.mp3']
+    });
+   
+  sound.play()
+}
+
+
+function playSrSound(){
+  var sound = new Howl({
+      src: ['./sound/Sr.mp3']
+    });
+   
+  sound.play()
+}
+
+function playYSound(){
+  var sound = new Howl({
+      src: ['./sound/Y.mp3']
+    });
+   
+  sound.play()
+}
+
+function playZrSound(){
+  var sound = new Howl({
+      src: ['./sound/Zr.mp3']
+    });
+   
+  sound.play()
+}
+function playNbSound(){
+  var sound = new Howl({
+      src: ['./sound/Nb.mp3']
+    });
+   
+  sound.play()
+}
+
+function playMoSound(){
+  var sound = new Howl({
+      src: ['./sound/Mo.mp3']
+    });
+   
+  sound.play()
+}
+
+function playTcSound(){
+  var sound = new Howl({
+      src: ['./sound/Tc.mp3']
+    });
+   
+  sound.play()
+}
+
+function playRuSound(){
+  var sound = new Howl({
+      src: ['./sound/Ru.mp3']
+    });
+   
+  sound.play()
+}
+function playRhSound(){
+  var sound = new Howl({
+      src: ['./sound/Rh.mp3']
+    });
+   
+  sound.play()
+
+}
+
+function playPdSound(){
+  var sound = new Howl({
+      src: ['./sound/Pd.mp3']
+    });
+   
+  sound.play()
+
+}
+
+function playAgSound(){
+  var sound = new Howl({
+      src: ['./sound/Ag.mp3']
+    });
+   
+  sound.play()
+
+}
+
+function playCdSound(){
+  var sound = new Howl({
+      src: ['./sound/Cd.mp3']
+    });
+   
+  sound.play()
+
+}
+
+function playInSound(){
+  var sound = new Howl({
+      src: ['./sound/In.mp3']
+    });
+   
+  sound.play()
+
+}
+
+function playSnSound(){
+  var sound = new Howl({
+      src: ['./sound/Sn.mp3']
+    });
+   
+  sound.play()
+
+}
+
+function playSbSound(){
+  var sound = new Howl({
+      src: ['./sound/Sb.mp3']
+    });
+   
+  sound.play()
+
+}
+
+function playTeSound(){
+  var sound = new Howl({
+      src: ['./sound/Te.mp3']
+    });
+   
+  sound.play()
+
+}
+function playISound(){
+  var sound = new Howl({
+      src: ['./sound/I.mp3']
+    });
+   
+  sound.play()
+
+}
+
+
+
+function playXeSound(){
+  var sound = new Howl({
+      src: ['./sound/Xe.mp3']
+    });
+   
+  sound.play()
+
+
+
+
+}
+
+
+function playCsSound(){
+  var sound = new Howl({
+      src: ['./sound/Cs.mp3']
+    });
+   
+  sound.play()
+
+}
+
+function playBaSound(){
+  var sound = new Howl({
+      src: ['./sound/Ba.mp3']
+    });
+   
+  sound.play()
+
+}
+
+function playLuSound(){
+  var sound = new Howl({
+      src: ['./sound/Lu.mp3']
+    });
+   
+  sound.play()
+
+}
+
+
+function playHfSound(){
+  var sound = new Howl({
+      src: ['./sound/Hf.mp3']
+    });
+   
+  sound.play()
+
+}
+
+
+
+function playTaSound(){
+  var sound = new Howl({
+      src: ['./sound/Ta.mp3']
+    });
+   
+  sound.play()
+
+}
+
+function playWSound(){
+  var sound = new Howl({
+      src: ['./sound/W.mp3']
+    });
+   
+  sound.play()
+
+}
+
+function playReSound(){
+  var sound = new Howl({
+      src: ['./sound/Re.mp3']
+    });
+   
+  sound.play()
+
+}
+
+function playOsSound(){
+  var sound = new Howl({
+      src: ['./sound/Os.mp3']
+    });
+   
+  sound.play()
+
+}
+
+
+function playIrSound(){
+  var sound = new Howl({
+      src: ['./sound/Ir.mp3']
+    });
+   
+  sound.play()
+
+}
+
+
+
+function playPtSound(){
+  var sound = new Howl({
+      src: ['./sound/Pt.mp3']
+    });
+   
+  sound.play()
+
+}
+
+
+function playAuSound(){
+  var sound = new Howl({
+      src: ['./sound/Au.mp3']
+    });
+   
+  sound.play()
+
+}
+
+function playHgSound(){
+  var sound = new Howl({
+      src: ['./sound/Hg.mp3']
+    });
+   
+  sound.play()
+
+}
+
+
+function playTiSound(){
+  var sound = new Howl({
+      src: ['./sound/Ti.mp3']
+    });
+   
+  sound.play()
+
+}
+
+function playPbSound(){
+  var sound = new Howl({
+      src: ['./sound/Pb.mp3']
+    });
+   
+  sound.play()
+
+}
+
+function playBiSound(){
+  var sound = new Howl({
+      src: ['./sound/Bi.mp3']
+    });
+   
+  sound.play()
+
+}
+
+function playPoSound(){
+  var sound = new Howl({
+      src: ['./sound/Po.mp3']
+    });
+   
+  sound.play()
+
+}
+
+function playAtSound(){
+  var sound = new Howl({
+      src: ['./sound/At.mp3']
+    });
+   
+  sound.play()
+
+}
+
+function playRnSound(){
+  var sound = new Howl({
+      src: ['./sound/Rn.mp3']
+    });
+   
+  sound.play()
+
+}
+
+function playFrSound(){
+  var sound = new Howl({
+      src: ['./sound/Fr.mp3']
+    });
+   
+  sound.play()
+
+}
+
+
+function playRaSound(){
+  var sound = new Howl({
+      src: ['./sound/Ra.mp3']
+    });
+   
+  sound.play()
+
+}
+function playLrSound(){
+  var sound = new Howl({
+      src: ['./sound/Lr.mp3']
+    });
+   
+  sound.play()
+
+}
+
+function playRfSound(){
+  var sound = new Howl({
+      src: ['./sound/Rf.mp3']
+    });
+   
+  sound.play()
+
+}
+function playDbSound(){
+  var sound = new Howl({
+      src: ['./sound/Db.mp3']
+    });
+   
+  sound.play()
+
+}
+
+function playSgSound(){
+  var sound = new Howl({
+      src: ['./sound/Sg.mp3']
+    });
+   
+  sound.play()
+
+}
+
+function playBhSound(){
+  var sound = new Howl({
+      src: ['./sound/Bh.mp3']
+    });
+   
+  sound.play()
+
+}
+
+function playHsSound(){
+  var sound = new Howl({
+      src: ['./sound/Hs.mp3']
+    });
+   
+  sound.play()
+
+}
+function playMtSound(){
+  var sound = new Howl({
+      src: ['./sound/Mt.mp3']
+    });
+   
+  sound.play()
+
+}
+
+
+function playDsSound(){
+  var sound = new Howl({
+      src: ['./sound/Ds.mp3']
+    });
+   
+  sound.play()
+
+}
+function playRgSound(){
+  var sound = new Howl({
+      src: ['./sound/Rg.mp3']
+    });
+   
+  sound.play()
+
+}
+
+function playCnSound(){
+  var sound = new Howl({
+      src: ['./sound/Cn.mp3']
+    });
+   
+  sound.play()
+
+}
+
+
+
+function playNhSound(){
+  var sound = new Howl({
+      src: ['./sound/Nh.mp3']
+    });
+   
+  sound.play()
+
+}
+
+function playFlSound(){
+  var sound = new Howl({
+      src: ['./sound/Fl.mp3']
+    });
+   
+  sound.play()
+
+}
+
+function playMcSound(){
+  var sound = new Howl({
+      src: ['./sound/Mc.mp3']
+    });
+   
+  sound.play()
+
+}
+
+function playLvSound(){
+  var sound = new Howl({
+      src: ['./sound/Lv.mp3']
+    });
+   
+  sound.play()
+
+}
+function playTsSound(){
+  var sound = new Howl({
+      src: ['./sound/Ts.mp3']
+    });
+   
+  sound.play()
+
+}
+
+function playOgSound(){
+  var sound = new Howl({
+      src: ['./sound/Og.mp3']
+    });
+   
+  sound.play()
+
+}
+
+function playLaSound(){
+  var sound = new Howl({
+      src: ['./sound/La.mp3']
+    });
+   
+  sound.play()
+
+}
+
+function playCeSound(){
+  var sound = new Howl({
+      src: ['./sound/Ce.mp3']
+    });
+   
+  sound.play()
+
+}
+
+
+function playPrSound(){
+  var sound = new Howl({
+      src: ['./sound/Pr.mp3']
+    });
+   
+  sound.play()
+
+}
+
+
+function playNdSound(){
+  var sound = new Howl({
+      src: ['./sound/Nd.mp3']
+    });
+   
+  sound.play()
+
+}
+
+
+function playPmSound(){
+  var sound = new Howl({
+      src: ['./sound/Pm.mp3']
+    });
+   
+  sound.play()
+
+}
+
+function playSmSound(){
+  var sound = new Howl({
+      src: ['./sound/Sm.mp3']
+    });
+   
+  sound.play()
+
+}
+
+function playEuSound(){
+  var sound = new Howl({
+      src: ['./sound/Eu.mp3']
+    });
+   
+  sound.play()
+
+}
+
+function playGdSound(){
+  var sound = new Howl({
+      src: ['./sound/Gd.mp3']
+    });
+   
+  sound.play()
+
+}
+
+function playTbSound(){
+  var sound = new Howl({
+      src: ['./sound/Tb.mp3']
+    });
+   
+  sound.play()
+
+}
+
+function playDySound(){
+  var sound = new Howl({
+      src: ['./sound/Dy.mp3']
+    });
+   
+  sound.play()
+
+}
+
+function playHoSound(){
+  var sound = new Howl({
+      src: ['./sound/Ho.mp3']
+    });
+   
+  sound.play()
+
+}
+
+
+function playErSound(){
+  var sound = new Howl({
+      src: ['./sound/Er.mp3']
+    });
+   
+  sound.play()
+
+}
+
+function playTmSound(){
+  var sound = new Howl({
+      src: ['./sound/Tm.mp3']
+    });
+   
+  sound.play()
+
+}
+
+function playYbSound(){
+  var sound = new Howl({
+      src: ['./sound/Yb.mp3']
+    });
+   
+  sound.play()
+
+}
+function playAcSound(){
+  var sound = new Howl({
+      src: ['./sound/Ac.mp3']
+    });
+   
+  sound.play()
+
+}
+
+
+function playThSound(){
+  var sound = new Howl({
+      src: ['./sound/Th.mp3']
+    });
+   
+  sound.play()
+
+}
+
+function playPaSound(){
+  var sound = new Howl({
+      src: ['./sound/Pa.mp3']
+    });
+   
+  sound.play()
+
+}
+
+function playUSound(){
+  var sound = new Howl({
+      src: ['./sound/U.mp3']
+    });
+   
+  sound.play()
+
+}
+
+
+function playNpSound(){
+  var sound = new Howl({
+      src: ['./sound/Np.mp3']
+    });
+   
+  sound.play()
+
+}
+
+
+function playPuSound(){
+  var sound = new Howl({
+      src: ['./sound/Pu.mp3']
+    });
+   
+  sound.play()
+
+}
+
+
+function playAmSound(){
+  var sound = new Howl({
+      src: ['./sound/Am.mp3']
+    });
+   
+  sound.play()
+
+}
+
+
+
+function playCmSound(){
+  var sound = new Howl({
+      src: ['./sound/Cm.mp3']
+    });
+   
+  sound.play()
+
+}
+
+
+function playBkSound(){
+  var sound = new Howl({
+      src: ['./sound/Bk.mp3']
+    });
+   
+  sound.play()
+
+  
+}
+
+
+function playCfSound(){
+  var sound = new Howl({
+      src: ['./sound/Cf.mp3']
+    });
+   
+  sound.play()
+
+}
+
+function playEsSound(){
+  var sound = new Howl({
+      src: ['./sound/Es.mp3']
+    });
+   
+  sound.play()
+
+}
+
+
+function playFmSound(){
+  var sound = new Howl({
+      src: ['./sound/Fm.mp3']
+    });
+   
+  sound.play()
+
+}
+
+
+function playMdSound(){
+  var sound = new Howl({
+      src: ['./sound/Md.mp3']
+    });
+   
+  sound.play()
+
+}
+
+function playNoSound(){
+  var sound = new Howl({
+      src: ['./sound/No.mp3']
+    });
+   
+  sound.play()
+
+}
